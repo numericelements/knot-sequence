40192ed4a41fc5d1c00e94731318cca0aa511fbb
jean-claude LEON <jeanclaude.leon2@gmail.com>
2025-02-13 16:38:25 +0100

    update of some source files after transfer from another project. Generation of the index.ts file containing the classes, interfaces, functions, to be accessible in the package.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,36 @@
 export { AbstractKnotIndex } from "./AbstractKnotIndex";
 export { AbstractKnotSequence } from "./AbstractKnotSequence";
+export { IncreasingOpenKnotSequenceClosedCurve } from "./IncreasingOpenKnotSequenceClosedCurve";
+export { IncreasingOpenKnotSequenceOpenCurve } from "./IncreasingOpenKnotSequenceOpenCurve";
+export { IncreasingPeriodicKnotSequenceClosedCurve } from "./IncreasingPeriodicKnotSequenceClosedCurve";
+export { Knot } from "./Knot";
+export { KnotIndexIncreasingSequence } from "./KnotIndexIncreasingSequence";
+export { KnotIndexStrictlyIncreasingSequence } from "./KnotIndexStrictlyIncreasingSequence";
+export { StrictlyIncreasingOpenKnotSequenceClosedCurve } from "./StrictlyIncreasingOpenKnotSequenceClosedCurve";
+export { StrictlyIncreasingOpenKnotSequenceOpenCurve } from "./StrictlyIncreasingOpenKnotSequenceOpenCurve";
+export { StrictlyIncreasingPeriodicKnotSequenceClosedCurve } from "./StrictlyIncreasingPeriodicKnotSequenceClosedCurve";
+
+
+
+// export interfaces
+export { IncreasingKnotSequenceInterface } from "./IncreasingKnotSequenceInterface";
+export { IncreasingOpenKnotSequenceInterface } from "./IncreasingOpenKnotSequenceInterface";
+export { KnotIndexInterface } from "./KnotIndexConstructorInterface";
+export { KnotSequenceInterface } from "./KnotSequenceInterface";
+export { StrictlyIncreasingKnotSequenceInterface } from "./StrictlyIncreasingKnotSequenceInterface";
+export { StrictlyIncreasingOpenKnotSequenceInterface } from "./StrictlyIncreasingOpenKnotSequenceInterface";
+
+
+// export decorators
+export { adaptParameterDecrementKnotMultiplicity } from "./KnotSequenceAndUtilities/adaptParameterDecrementKnotMultiplicity";
+export { adaptParameterInsertKnot } from "./KnotSequenceAndUtilities/adaptParameterInsertKnot";
+export { adaptParameterRaiseKnotMultiplicity } from "./KnotSequenceAndUtilities/adaptParameterRaiseKnotMultiplicity";
+
+// export functions
+export { fromIncreasingOpentoIncreasingPeriodicKnotSequence } from "./KnotSequenceAndUtilities/fromIncreasingOpentoIncreasingPeriodicKnotSequence";
+export { fromIncreasingPeriodicToIncreasingOpenKnotSequenceCC } from "./KnotSequenceAndUtilities/fromIncreasingPeriodicToIncreasingOpenKnotSequenceCC";
+export { fromIncreasingPeriodicToStrictlyIncreasingPeriodicKnotSequence } from "./KnotSequenceAndUtilities/fromIncreasingPeriodicToStrictlyIncreasingPeriodicKnotSequence";
+export { fromIncreasingToStrictlyIncreasingOpenKnotSequenceCC } from "./KnotSequenceAndUtilities/fromIncreasingToStrictlyIncreasingOpenKnotSequenceCC";
+export { fromIncreasingToStrictlyIncreasingOpenKnotSequenceOC } from "./KnotSequenceAndUtilities/fromIncreasingToStrictlyIncreasingOpenKnotSequenceOC";
+export { fromInputParametersToIncreasingOpenKnotSequenceCC } from "./KnotSequenceAndUtilities/fromInputParametersToIncreasingOpenKnotSequenceCC";
+
