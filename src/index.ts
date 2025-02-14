3b6afd1e1e3ce75f218e6f0da8392161e1d13b6f
Eric Demers <eric.demers@polymtl.ca>
2025-02-13 21:03:56 -0500

    update to package.json and tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,6 @@ export { StrictlyIncreasingOpenKnotSequenceClosedCurve } from "./StrictlyIncreas
 export { StrictlyIncreasingOpenKnotSequenceOpenCurve } from "./StrictlyIncreasingOpenKnotSequenceOpenCurve";
 export { StrictlyIncreasingPeriodicKnotSequenceClosedCurve } from "./StrictlyIncreasingPeriodicKnotSequenceClosedCurve";
 
-
-
 // export interfaces
 export { IncreasingKnotSequenceInterface } from "./IncreasingKnotSequenceInterface";
 export { IncreasingOpenKnotSequenceInterface } from "./IncreasingOpenKnotSequenceInterface";
@@ -19,7 +17,6 @@ export { KnotIndexInterface } from "./KnotIndexConstructorInterface";
 export { KnotSequenceInterface } from "./KnotSequenceInterface";
 export { StrictlyIncreasingKnotSequenceInterface } from "./StrictlyIncreasingKnotSequenceInterface";
 export { StrictlyIncreasingOpenKnotSequenceInterface } from "./StrictlyIncreasingOpenKnotSequenceInterface";
-
 
 // export decorators
 export { adaptParameterDecrementKnotMultiplicity } from "./KnotSequenceAndUtilities/adaptParameterDecrementKnotMultiplicity";
@@ -34,3 +31,6 @@ export { fromIncreasingToStrictlyIncreasingOpenKnotSequenceCC } from "./KnotSequ
 export { fromIncreasingToStrictlyIncreasingOpenKnotSequenceOC } from "./KnotSequenceAndUtilities/fromIncreasingToStrictlyIncreasingOpenKnotSequenceOC";
 export { fromInputParametersToIncreasingOpenKnotSequenceCC } from "./KnotSequenceAndUtilities/fromInputParametersToIncreasingOpenKnotSequenceCC";
 
+// export Error Messages
+export * from "./ErrorMessages/Knots";
+export * from "./ErrorMessages/KnotSequences";
