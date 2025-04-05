f3149ca6d2dfe1d5f06464ad8544f32b29353755
jean-claude LEON <jeanclaude.leon2@gmail.com>
2025-04-05 21:38:04 +0200

    update of the knot-sequence package with simple test files to validate the access to the package through imports. Test.mjs enables access through a javascript test file. test.ts and test.ts obtained after compilation with tsc. Test files are located in test-package directory.

--- test-package/test.js
+++ test-package/test.js
@@ -1,15 +1,16 @@
 import { StrictlyIncreasingOpenKnotSequenceOpenCurve } from '@numericelements/knot-sequence';
 import { NO_KNOT_OPEN_CURVE } from '@numericelements/knot-sequence';
-
 console.log('Class imported:', typeof StrictlyIncreasingOpenKnotSequenceOpenCurve);
 try {
     const instance = new StrictlyIncreasingOpenKnotSequenceOpenCurve(3, { type: NO_KNOT_OPEN_CURVE });
     console.log('Instance created successfully');
-} catch (error) {
+}
+catch (error) {
     if (error instanceof Error) {
         console.error('Error creating instance:', error.message);
         console.error('Error stack:', error.stack);
-      } else {
+    }
+    else {
         console.error('Unknown error:', error);
-      }
+    }
 }
--- test-package/test.mjs
+++ test-package/test.mjs
@@ -5,11 +5,6 @@ console.log('Class imported:', typeof StrictlyIncreasingOpenKnotSequenceOpenCurv
 try {
     const instance = new StrictlyIncreasingOpenKnotSequenceOpenCurve(3, { type: NO_KNOT_OPEN_CURVE });
     console.log('Instance created successfully');
-} catch (error) {
-    if (error instanceof Error) {
-        console.error('Error creating instance:', error.message);
-        console.error('Error stack:', error.stack);
-      } else {
-        console.error('Unknown error:', error);
-      }
-}
+  } catch (error) {
+    console.error('Error creating instance:', error.message);
+  }
